2fd610111c3432136f1fa987feffc39d7bac989e
josephkim93 <168089648+frogKim93@users.noreply.github.com>
2024-05-13 00:40:24 +0900

    chore: CORS설정

--- src/main/java/com/frogkim93/stationsystemapi/config/WebConfig.java
+++ src/main/java/com/frogkim93/stationsystemapi/config/WebConfig.java
@@ -2,15 +2,13 @@ package com.frogkim93.stationsystemapi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("*")
+        registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("*");
     }
--- src/main/java/com/frogkim93/stationsystemapi/login/controller/LoginController.java
+++ src/main/java/com/frogkim93/stationsystemapi/login/controller/LoginController.java
@@ -6,10 +6,7 @@ import com.frogkim93.stationsystemapi.login.service.LoginService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
